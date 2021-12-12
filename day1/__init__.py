9af0161fcbaaf018b06470d0193170935521dd2a
Stephan Delhey <stephan.delhey@hotmail.de>
2021-12-12 21:54:45 +0100

    add day 1

---
+++ day1/__init__.py
@@ -0,0 +1,36 @@
+filename = 'data'
+
+# Part 1
+with open(filename) as file:
+    prev = None
+    counter = 0
+    for line in file:
+        line = int(line)
+
+        if prev is not None and line > prev:
+            counter += 1
+
+        prev = line
+
+    print(counter)
+
+
+# Part 2
+with open(filename) as file:
+    size = 3
+    prev = None
+    counter = 0
+
+    lines = file.readlines()
+    lines = [int(line) for line in lines]
+
+    for i in range(len(lines) - size + 1):
+        current_lines = lines[i:i + size]
+        current_lines_sum = sum(current_lines)
+
+        if prev is not None and current_lines_sum > prev:
+            counter += 1
+
+        prev = current_lines_sum
+
+    print(counter)
