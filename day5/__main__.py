619159357555a07db5d3e1422f331a8905b167fc
Stephan Delhey <stephan.delhey@hotmail.de>
2021-12-18 23:50:59 +0100

    add day5 part 2

--- day5/__main__.py
+++ day5/__main__.py
@@ -6,16 +6,41 @@ class Point:
         self.x = int(point_as_str.split(',')[0])
         self.y = int(point_as_str.split(',')[1])
 
-    def distance_to(self, other_point: "Point"):
-        pass
-
     def __str__(self):
         return f"{self.x},{self.y}"
 
 
-# def add_vents_to_result(constant_axis: str, p1: Point, p2: Point):
-#     const = p1.x if constant_axis is 'x' else p1.y
-#     min =
+def add_horizontal_or_vertical_line(output: dict, p1: Point, p2: Point):
+    start = min(p1.x, p2.x) if p1.x != p2.x else min(p1.y, p2.y)
+    end = max(p1.x, p2.x) if p1.x != p2.x else max(p1.y, p2.y)
+
+    for i in range(start, end + 1):
+        position = f"{p1.x if p1.x == p2.x else i},{p1.y if p1.y == p2.y else i}"
+
+        if position in output:
+            output[position] += 1
+        else:
+            output[position] = 1
+
+
+def add_diagonal_lines(output: dict, p1: Point, p2: Point):
+    length = abs(p1.x - p2.x) + 1
+    for i in range(length):
+        if p1.x < p2.x:
+            x_pos = p1.x + i
+        else:
+            x_pos = p1.x - i
+
+        if p1.y < p2.y:
+            y_pos = p1.y + i
+        else:
+            y_pos = p1.y - i
+
+        position = f"{x_pos},{y_pos}"
+        if position in output:
+            output[position] += 1
+        else:
+            output[position] = 1
 
 
 with open(filename) as file:
@@ -25,31 +50,11 @@ with open(filename) as file:
         p1 = Point(line.split(' ')[0])
         p2 = Point(line.split(' ')[2])
 
-        if p1.x == p2.x:
-            x = p1.x
-            y1 = min(p1.y, p2.y)
-            y2 = max(p1.y, p2.y)
+        if p1.x == p2.x or p1.y == p2.y:
+            add_horizontal_or_vertical_line(result, p1, p2)
 
-            for i in range(y1, y2 + 1):
-                position = f"{x},{i}"
+        else:
+            add_diagonal_lines(result, p1, p2)
 
-                if position in result:
-                    result[position] += 1
-                else:
-                    result[position] = 1
-        elif p1.y == p2.y:
-            y = p1.y
-            x1 = min(p1.x, p2.x)
-            x2 = max(p1.x, p2.x)
-
-            for i in range(x1, x2 + 1):
-                position = f"{i},{y}"
-
-                if position in result:
-                    result[position] += 1
-                else:
-                    result[position] = 1
-
-    print(result)
     final = [k for k, v in result.items() if v >= 2]
     print(len(final))
